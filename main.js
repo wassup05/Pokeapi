7dc0242421ab3f580ab1deec0cf9d7976b599e6d
wassup05 <160420354+wassup05@users.noreply.github.com>
2024-06-29 19:20:50 +0530

    changed the image name

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const argc = process.argv.length;
 
 if (argc == 3) {
   const func = require('./src/function');
-  func(process.argv[2].toLowerCase(),'images/sprite.png') // making the search case-insensitive
+  func(process.argv[2].toLowerCase(),'./sprite.png') // making the search case-insensitive
 } else {
   console.log("Invalid arguments supplied!");
   process.exit(2);
